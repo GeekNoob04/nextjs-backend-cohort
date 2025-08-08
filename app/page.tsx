6047434bb82df7191de67ba058dbc440506b56dd
GeekNoob04 <harshitbudhraja0@gmail.com>
2025-08-09 04:50:49 +0530

    Data fetching in Next

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-    return <div>Hi there</div>;
+import axios from "axios";
+
+async function fetchData() {
+    const res = await axios.get(
+        "https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details"
+    );
+    return res.data;
+}
+export default async function Home() {
+    const userDetails = await fetchData();
+    return (
+        <div>
+            {userDetails.email}
+            {userDetails.name}
+        </div>
+    );
 }
