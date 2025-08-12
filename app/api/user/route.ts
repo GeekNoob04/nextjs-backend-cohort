9bf6bfde21ae20beee7cc818488c6b0f02c72d80
GeekNoob04 <harshitbudhraja0@gmail.com>
2025-08-13 02:31:06 +0530

    backend in nextjs

---
+++ app/api/user/route.ts
@@ -0,0 +1,6 @@
+export function GET() {
+    return Response.json({
+        email: "harshitbudhraja0@gmail.com",
+        name: "harshit",
+    });
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 async function fetchData() {
-    await new Promise((r) => setTimeout(r, 2000));
-    const res = await axios.get(
-        "https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details"
-    );
+    const res = await axios.get("http://localhost:3000/api/user");
     return res.data;
 }
 export default async function Home() {
